faf4a2b326ee97f889e3efde1a6f402c1017621a
Bader Kharbat <baderkharbat@Baders-MacBook-Air.local>
2022-07-18 12:35:31 -0400

    init

---
+++ cam.py
@@ -0,0 +1,64 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import uuid
+import os
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+def calculate_angle(a,b,c):
+    a = np.array(a) # First
+    b = np.array(b) # Mid
+    c = np.array(c) # End
+    
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+    
+    if angle >180.0:
+        angle = 360-angle
+        
+    return angle 
+
+cap = cv2.VideoCapture(0)
+## Setup mediapipe instance
+with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands: 
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            
+            # Recolor image to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+        
+            # Make detection
+            body_results = pose.process(image)
+            hand_results = hands.process(image)
+        
+            # Recolor back to BGR
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            
+            
+            # Render detections
+            mp_drawing.draw_landmarks(image, body_results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                    )       
+            if hand_results.multi_hand_landmarks:
+                for num, hand in enumerate(hand_results.multi_hand_landmarks):
+                    mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS, 
+                                            mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                                            mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2),
+                                            )
+                    
+            
+            cv2.imshow('Mediapipe Feed', image)
+
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+
+    cap.release()
+    cv2.destroyAllWindows()
